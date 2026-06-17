f03efd492153b4cac274a7c602b67da2d1c43079
Henry Sun <hs325@duke.edu>
2026-06-17 16:17:50 +0000

    chosen GEBV start

---
+++ vis/gebv_chosen.R
@@ -0,0 +1,8 @@
+# TODO compute avg GEBV and gain for surviving oysters
+
+# compute expected number of surviving oysters given # of missing tags
+# compute how many were chosen by each model
+
+# 22N9 
+
+# 21DW
